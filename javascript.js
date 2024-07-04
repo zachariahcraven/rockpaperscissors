01792c68a4f4eb28575d4924e9acfe95a7aaa020
zachariahcraven <118693079+zachariahcraven@users.noreply.github.com>
2024-07-03 20:52:13 -0600

    update js with working functions for gameplay

--- javascript.js
+++ javascript.js
@@ -5,12 +5,51 @@ function getComputerChoice() {
     let val = Math.floor(Math.random() * 3)
     switch (val) {
         case 0:
-            return "Rock";
+            return "rock";
         case 1:
-            return "Paper";
+            return "paper";
         case 2:
-            return "Scissors";
+            return "scissors";
     }
 }
 
-console.log(getComputerChoice());
+//function returns (string) the result of the game or prompts user for new choice 
+function getUserInput() {
+    let input = prompt("Enter Choice");
+    input = input.toLowerCase();
+    if (input == "rock" || input == "scissors" || input == "paper") {
+        return input;
+    } else { 
+        return "Invalid Input Please try again."
+    }
+}
+
+//Returns a string (result of the match)
+function outcome(userAnwser, computerAnwser) {
+    console.log(userAnwser, "vs", computerAnwser);
+    if (userAnwser == computerAnwser) {
+        return "tie"
+    }
+    switch (userAnwser) {
+        case "paper":
+            if (computerAnwser == "scissors") {
+                return "you lose"
+            }else {
+                return "you won"
+            }
+        case "rock":
+            if (computerAnwser == "paper") {
+                return "you lose"
+            }else {
+                return "you won"
+            }
+        case "scissors":
+            if (computerAnwser == "rock") {
+                return "you lose"
+            }else {
+                return "you won"       
+            }
+    }
+}
+
+console.log(outcome(getUserInput(), getComputerChoice()));
